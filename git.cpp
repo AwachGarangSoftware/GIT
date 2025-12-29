3706887624ffde5f8f0c6c3606557ba257807a8e
AwachGarangSoftware <awachgarang276@gmail.com>
2025-12-29 10:12:09 -0800

    added features

--- git.cpp
+++ git.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
-    cout << "setting up git is challenging but lovely!";
+    cout << "setting up git is challenging but lovely!!!";
+    cout << "Hello ,world !";
+    cout << "Hey there ! how are you doing ?";
     return 0;
 }
